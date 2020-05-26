721cb59896a1c454b121b538fef1c835f50c983d
e <liuyangyang@shenkeinfo.com>
2020-05-26 15:37:58 +0800

    CoController

---
+++ app/Controller/CoController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyy
+ * Date: 2020/5/26
+ * Time: 15:34
+ */
+
+namespace App\Controller;
+use Hyperf\HttpServer\Annotation\AutoController;
+use Hyperf\HttpServer\Contract\RequestInterface;
+
+/**
+ * @AutoController()
+ */
+class CoController
+{
+    private $foo = 1;
+
+    public function get()
+    {
+        return $this->foo;
+    }
+
+    public function index(RequestInterface $request)
+    {
+        $foo = $request->input("foo");
+        $this->foo = $foo;
+        return $this->foo;
+    }
+}
