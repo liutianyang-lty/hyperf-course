930d36042ebdb7a3214b55056d5caabc9c985cfd
e <liuyangyang@shenkeinfo.com>
2020-05-26 13:55:52 +0800

    Aspect

---
+++ app/Aspect/IndexAspect.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyy
+ * Date: 2020/5/26
+ * Time: 13:51
+ */
+
+namespace App\Aspect;
+
+
+use App\Controller\IndexController;
+use Hyperf\Di\Annotation\Aspect;
+use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
+
+/**
+ * @Aspect()
+ */
+class IndexAspect extends AbstractAspect
+{
+    //定义要切入的类
+    public $classes = [
+        IndexController::class . "::" . "index",
+    ];
+
+    public function process(ProceedingJoinPoint $proceedingJoinPoint)
+    {
+        var_dump(__CLASS__);
+        // TODO: Implement process() method.
+        $result = $proceedingJoinPoint->process();
+        return $result;
+    }
+}
