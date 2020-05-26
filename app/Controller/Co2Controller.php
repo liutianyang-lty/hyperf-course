35b17ce816f7bcf96e9117c450ffdf1181839b73
e <liuyangyang@shenkeinfo.com>
2020-05-26 16:01:54 +0800

    test

--- app/Controller/Co2Controller.php
+++ app/Controller/Co2Controller.php
@@ -3,44 +3,39 @@
  * Created by PhpStorm.
  * User: lyy
  * Date: 2020/5/26
- * Time: 15:34
+ * Time: 15:58
  */
 
 namespace App\Controller;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Contract\RequestInterface;
-use Hyperf\Utils\Context;
 
 /**
  * @AutoController()
  * @property int $foo
  */
-class CoController
+class Co2Controller
 {
+    /**
+     * @Inject()
+     * @var \App\Foo
+     */
+    private $foo;
+
     public function get()
     {
         //return Context::get('foo', 'null'); // 协程上下文获取 防止协程之间数据混淆
-        return $this->foo;
+        return $this->foo->bar;
     }
 
     public function update(RequestInterface $request)
     {
         $foo = $request->input("foo");
         //Context::set('foo', $foo);
-        $this->foo = $foo;
+        $this->foo->bar = $foo;
         //return Context::get('foo');
-        return $this->foo;
+        return $this->foo->bar;
     }
 
-    public function __get($name)
-    {
-        // TODO: Implement __get() method.
-        return Context::get(__CLASS__ . ':' . $name);
-    }
-
-    public function __set($name, $value)
-    {
-        // TODO: Implement __set() method.
-        Context::set(__CLASS__ . ':' . $name, $value);
-    }
 }
---
+++ app/Foo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lyy
+ * Date: 2020/5/26
+ * Time: 15:56
+ */
+
+namespace App;
+
+
+class Foo
+{
+    public $bar = 1;
+}
